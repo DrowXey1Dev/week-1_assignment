93d1838c3f9df64eceb88cbbdc5fb411da619d9c
DrowXey1Dev <112534187+DrowXey1Dev@users.noreply.github.com>
2025-09-02 23:52:54 -0400

    Update apputil.py
    pushing changes for exercise 2

--- apputil.py
+++ apputil.py
@@ -52,5 +52,72 @@ user_input = re.sub(r'\s', '', user_input)
 #call function
 palindrome_detector(user_input)
 
+#-------------------------------------------------------------------------------------------------------------------------------#
+#EXERCISE 2
+
+#-----IMPORTS-----#
+import re
+
+#-----FUNCTION DEFS-----#
+def palindrome_detector(user_input):
+
+    #debugging code
+    print("User entered the following text: " + user_input)
+    print("Length of user entered text is: ", len(user_input))
+
+    #set pointer positions
+    #set at starting index of array
+    left_pointer = 0
+    #set at the end index of array
+    right_pointer = len(user_input) - 1
+
+    
+
+    #compare characters at each end cycling inwards
+    while left_pointer < right_pointer:
+        if user_input[left_pointer] != user_input[right_pointer]:
+            print("This text is NOT a palindrome")
+            return False
+        left_pointer += 1
+        right_pointer -= 1
+
+    print("This text IS a palindrome")
+    return True
+
+def parentheses_balancer(user_input):
+    #return true if input parentheses are balanced
+
+    #logic...
+
+
+
+
+
+    
+#-----------------------#
+
+
+
+
+#-----MAIN-----#
+user_input = input("Enter a string to be tested: ")
+user_input = user_input.lower()
+
+
+#puncuation is removed and replaced by the empty string
+user_input = re.sub(r'[^\w\s]', '', user_input)
+
+#spaces are replaceed with the empty string
+user_input = re.sub(r'\s', '', user_input)
+
+#call function
+palindrome_detector(user_input)
+
+#call parentheses function
+print("The following parentheses are balanced (T/F):", parentheses_balancer(user_input))
+
+
+
+
 
 
